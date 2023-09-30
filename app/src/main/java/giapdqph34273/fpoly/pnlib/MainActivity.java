17dda984ee7b3f517b0bd41612c40cec28b01910
doquanggiap <giapdqph34273@fpt.edu.vn>
2023-09-30 08:13:19 +0700

    hellow

---
+++ app/src/main/java/giapdqph34273/fpoly/pnlib/MainActivity.java
@@ -0,0 +1,25 @@
+package giapdqph34273.fpoly.pnlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, dangNhap.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
---
+++ app/src/main/java/giapdqph34273/fpoly/pnlib/quanlysanpham.java
@@ -0,0 +1,45 @@
+package giapdqph34273.fpoly.pnlib;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class quanlysanpham extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quanlysanpham);
+    }
+//    private void setUpToolbar() {
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_icon);
+//        getSupportActionBar().setTitle("Quản lý bán hàng");
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                if (item.getItemId()==R.id.qlsp){
+//                    drawerLayout.close();
+//                } else if (item.getItemId()==R.id.gioithieu) {
+//                    Intent intent = new Intent(quanLySanPham.this, gioiThieu.class);
+//                    startActivity(intent);
+//                } else if (item.getItemId()==R.id.caidat) {
+//                    caiDatDialog();
+//                } else if (item.getItemId()==R.id.dangxuat) {
+//                    dialog_dangxuat();
+//                }
+//                return false;
+//            }
+//        });
+//    }
+}
