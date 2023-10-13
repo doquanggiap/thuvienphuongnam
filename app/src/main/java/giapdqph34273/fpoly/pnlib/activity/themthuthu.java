d730688253821dea32a5edf249706715aaa7ebc7
doquanggiap <giapdqph34273@fpt.edu.vn>
2023-10-13 23:10:50 +0700

    cập nhật dự án

--- app/src/main/java/giapdqph34273/fpoly/pnlib/activity/themthuthu.java
+++ app/src/main/java/giapdqph34273/fpoly/pnlib/activity/themthuthu.java
@@ -1,12 +1,25 @@
 package giapdqph34273.fpoly.pnlib.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import giapdqph34273.fpoly.pnlib.DAO.ThuThuDAO;
 import giapdqph34273.fpoly.pnlib.R;
@@ -17,6 +30,12 @@ public class themthuthu extends AppCompatActivity {
     EditText edtUser,edtHoten,edtPass,edtNhapLai;
     Button btnLuu;
     private ThuThuDAO thuThuDAO;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +43,7 @@ public class themthuthu extends AppCompatActivity {
         setContentView(R.layout.activity_themthuthu);
 
         anhxa();
+        setUpToolbar();
 
         btnLuu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,5 +87,87 @@ public class themthuthu extends AppCompatActivity {
         edtNhapLai = findViewById(R.id.edtNhapLai);
         btnLuu = findViewById(R.id.btnLuu);
         thuThuDAO = new ThuThuDAO(this);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.my_toolbar);
+        navigationView = findViewById(R.id.navigationView);
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_icon);
+        getSupportActionBar().setTitle("Quản lý loại sách");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.qlpm) {
+                    Intent intent = new Intent(themthuthu.this, quanlyphieumuon.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.qlls) {
+                    Intent intent = new Intent(themthuthu.this, quanlyloaisach.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.qls) {
+                    Intent intent = new Intent(themthuthu.this, quanlysach.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.qltv) {
+                    Intent intent = new Intent(themthuthu.this, quanlythanhvien.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.topten) {
+                    Intent intent = new Intent(themthuthu.this, Top10Sach.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.doanhthu) {
+                    Intent intent = new Intent(themthuthu.this, tongDoanhThu.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.themNguoiDung) {
+                    drawerLayout.close();
+                } else if (item.getItemId() == R.id.doiMatKhau) {
+                    Intent intent = new Intent(themthuthu.this, doiMatKhau.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.dangxuat) {
+                    dialog_dangxuat();
+                }
+                return false;
+            }
+        });
+    }
+    public void dialog_dangxuat() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.baseline_question_mark_24);
+        builder.setCancelable(false);
+        builder.setTitle("Đăng xuất");
+        builder.setMessage("Bạn có muốn đăng xuất không?");
+        builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Bắt sự kiện nhấn nút Có
+                SharedPreferences sharedPreferences = getSharedPreferences("thongtin", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", false);
+                editor.apply();
+                Intent intent = new Intent(themthuthu.this, dangNhap.class);
+
+                // Đặt cờ FLAG_ACTIVITY_NEW_TASK để tạo một nhiệm vụ mới
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Bắt sự kiện nhấn nút Không
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
